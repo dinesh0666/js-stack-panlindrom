bc8fc99f033018b3c5ce45dfdc9340f5d118274f
dinesh0666 <dineshkumar06@yahoo.com>
2019-07-08 20:09:11 +0530

    1 file modified

--- index.js
+++ index.js
@@ -26,3 +26,39 @@ if (rword === word) {
 else {
    console.log(word + " is not a palindrome.");
 }
+
+//creating stack
+var stack = function(value){
+  this.count =0;
+  this.storage={};
+      
+    // Adds a value onto the end of the stack
+    this.push = function(value) {
+        this.storage[this.count] = value;
+        this.count++;
+    }
+    this.size = function() {
+        return this.count;
+    }
+      // Returns the value at the end of the stack
+    this.peek = function() {
+        return this.storage[this.count-1];
+    }
+    // Removes and returns the value at the end of the stack
+    this.pop = function() {
+        if (this.count === 0) {
+            return undefined;
+        }
+        this.count--;
+        var result = this.storage[this.count];
+        delete this.storage[this.count];
+        return result;
+    }
+
+var myStack = new Stack();
+
+myStack.push(1);
+myStack.push(2);
+console.log(myStack.peek());
+
+}
